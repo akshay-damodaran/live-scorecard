b567374577ef769bacf7b8f2dbdf48d53115a5f2
Akshay.Damodaran <akshay.damodaran@ril.com>
2018-12-18 14:26:38 +0530

    Added bassic routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './containers/Home';
+import Admin from './containers/Admin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+      <Switch>
+          <Route exact path="/admin" component={Admin} />
+          <Route path="/" component={Home} />
+          <Redirect to="/" />
+      </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
