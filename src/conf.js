26aa36499b00deab65573a1a31b36ae154dca651
Akshay.Damodaran <akshay.damodaran@ril.com>
2019-01-19 14:14:54 +0530

    seperated local and prod conf

--- src/conf.js
+++ src/conf.js
@@ -1,5 +1,15 @@
+// Local conf
 const conf = {
   base_url: 'http://localhost:4000/',
-}
+  broadcast_socket_url: 'http://localhost:4001',
+  receiver_socket_url: 'http://localhost:4002',
+};
+
+// Prod conf
+// const conf = {
+//   base_url: 'https://livescorecardserver.herokuapp.com:4000/',
+//   broadcast_socket_url: 'https://livescorecardserver.herokuapp.com:4001',
+//   receiver_socket_url: 'https://livescorecardserver.herokuapp.com:4002',
+// };
 
 export default conf;
